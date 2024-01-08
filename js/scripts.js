8a39c1f2e77a2cc5baa984bfc6f7c1ffc3fca7db
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-01-08 15:18:31 -0500

    Created work folders and files
    Created CSS, JS and Image folders. Developed basic HTML, CSS & JS files.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: 'Charizard', height: 1.7, types: ['sky','fire']},
+  {name: 'Tyranitar', height: 2, types: ['land','rock']},
+  {name: 'Sharpedo', height: 1.8, types: ['water','sea']}
+];
