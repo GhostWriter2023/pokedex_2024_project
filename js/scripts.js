ade84e9f7e1cb0078c7dcd945a7ddea0ec0a9728
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-01-11 18:30:02 -0500

    Visual and JS array processing
    Visual presentation:  styles, fonts, colour.
    Page layout: header, title, logo
    Array processing: display data, conditional check height value

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 let pokemonList = [
-  {name: 'Charizard', height: 1.7, types: ['sky','fire']},
-  {name: 'Tyranitar', height: 2, types: ['land','rock']},
-  {name: 'Sharpedo', height: 1.8, types: ['water','sea']}
+  { name: 'Charizard', height: 1.7, types: ['sky', 'fire'] },
+  { name: 'Tyranitar', height: 2, types: ['land', 'rock'] },
+  { name: 'Sharpedo', height: 1.8, types: ['water', 'sea'] }
 ];
+
+// For Loop to cycle through each item in pokemonList followed by command to display
+for (let i = 0; i < pokemonList.length; i++) {
+  let threePokemon = pokemonList[i];
+  // Function to check if the height is above 1.9
+  let tallPokemon = threePokemon.height > 1.9;
+  if (tallPokemon) {
+    document.write('<strong>' + threePokemon.name + ' (height: ' + threePokemon.height + ') - Wow, that’s big!</strong><br>');
+  } else {
+    document.write(threePokemon.name + ' (height: ' + threePokemon.height + ')<br>');
+  }
+}
