4035c4f756e1e87ab2331d611884a6301b9336b5
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-01-18 16:55:52 -0500

    Updated "for" to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,22 @@ let pokemonList = [
   { name: 'Sharpedo', height: 1.8, types: ['water', 'sea'] }
 ];
 
-// For Loop to cycle through each item in pokemonList followed by command to display
+// Updated to a forEach function to cycle through each item in pokemonList followed by command to display
+pokemonList.forEach((pokemon) => {
+  // Function to check if the height is above 1.9
+  let tallPokemon = pokemon.height > 1.9;
+
+  if (tallPokemon) {
+    document.write(`<strong>${pokemon.name} (height: ${pokemon.height}) - Wow, that’s big!</strong><br>`);
+  } else {
+    document.write(`${pokemon.name} (height: ${pokemon.height})<br>`);
+  }
+});
+
+/* Original For Loop
 for (let i = 0; i < pokemonList.length; i++) {
   let threePokemon = pokemonList[i];
-  // Function to check if the height is above 1.9
+
   let tallPokemon = threePokemon.height > 1.9;
   if (tallPokemon) {
     document.write('<strong>' + threePokemon.name + ' (height: ' + threePokemon.height + ') - Wow, that’s big!</strong><br>');
@@ -15,3 +27,5 @@ for (let i = 0; i < pokemonList.length; i++) {
     document.write(threePokemon.name + ' (height: ' + threePokemon.height + ')<br>');
   }
 }
+*/
+
