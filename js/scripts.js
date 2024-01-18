204e32b8e8b1c746251f9d5596bb11fea49e84fa
GhostWriter2023 <154560842+GhostWriter2023@users.noreply.github.com>
2024-01-18 18:28:48 -0500

    Set an IIFE wrapped list + entry validation tool
    Created:
    1) a new IIFE repository variable;
    2) a function to validate Pokemon entries (no missing data).

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,37 @@
-let pokemonList = [
-  { name: 'Charizard', height: 1.7, types: ['sky', 'fire'] },
-  { name: 'Tyranitar', height: 2, types: ['land', 'rock'] },
-  { name: 'Sharpedo', height: 1.8, types: ['water', 'sea'] }
-];
+let pokemonRepository = (function () {
+  // Set up a private variable to store and control access to the pokemonList array
+  let pokemonList = [
+    { name: 'Charizard', height: 1.7, types: ['sky', 'fire'] },
+    { name: 'Tyranitar', height: 2, types: ['land', 'rock'] },
+    { name: 'Sharpedo', height: 1.8, types: ['water', 'dark'] }
+  ];
 
-// Updated to a forEach function to cycle through each item in pokemonList followed by command to display
-pokemonList.forEach((pokemon) => {
+  // Public functions
+  return {
+    getAll: function () {
+      return pokemonList;
+    },
+    add: function (item) {
+      // Check if entered item is a valid Pokemon object
+      if (typeof item === 'object' && item.hasOwnProperty('name') && item.hasOwnProperty('height') && item.hasOwnProperty('types')) {
+        pokemonList.push(item);
+        console.log('${item.name} added to the pokemonList.');
+      } else {
+        console.error('Invalid Pokemon object. Please provide a valid Pokemon with name, height, and types.');
+      }
+    }
+  };
+})();
+
+// Adding a new Pokemon
+let newPokemon = { name: 'Pikachu', height: 0.4, types: ['water', 'electric'] };
+pokemonRepository.add(newPokemon);
+
+// Test to observe the validity function work by adding another Pokemon, but with data missing
+let secondPokemon = {height: 0.4, types: ['water', 'electric'] };
+pokemonRepository.add(secondPokemon);
+
+  pokemonRepository.getAll().forEach((pokemon) => {
   // Function to check if the height is above 1.9
   let tallPokemon = pokemon.height > 1.9;
 
